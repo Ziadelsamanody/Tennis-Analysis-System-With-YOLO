4b6a2cd5e02201997ef18bac3efce85c4091ae3a
Ziadelsamanody <ziadelsamanody@gmail.com>
2025-10-17 22:53:56 +0300

    Save current changes before history rewrite

--- yoloinference.py
+++ yoloinference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x.pt')
 
-result = model.predict('input_videos/input_video.mp4', conf=0.2, save=True)
+result = model.track('input_videos/input_video.mp4', conf=0.2, save=True)
 
 
 
