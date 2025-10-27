f3fd0c5abd40c7fde03645707d79a0dded6122bf
Ziadelsamanody <ziadelsamanody@gmail.com>
2025-10-27 21:23:13 +0300

    add ball tracker handle ball detection stubs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils import (read_video, save_video) 
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
 
 def main():
     # read video
@@ -11,9 +11,14 @@ def main():
     player_detection = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path='tracker_stubs/player_detection.pkl')
   
     
+    # detect ball 
+    ball_tracker = BallTracker('models/yolov5last.pt')
+    ball_detection = ball_tracker.detect_frames(video_frames, read_from_stub=False, stub_path='tracker_stubs/ball_detection.pkl')
+
     #draw output 
-    #draw player Bounding boxes
+    #draw player and ball  Bounding boxes
     output_video_frames = player_tracker.draw_boxes(video_frames, player_detection)
+    output_video_frames = ball_tracker.draw_boxes(video_frames, ball_detection)
 
     save_video(output_video_frames, 'output_videos/output_video.avi')
 
